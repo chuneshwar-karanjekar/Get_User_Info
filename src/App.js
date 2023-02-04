31adecb1ee71e99f34f76305299d54185a922b47
chuneshwar karanjekar <001diamondsgroup@gmail.com>
2023-02-04 14:39:28 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React, { useState } from "react";
+import './App.css';
+import axios from 'axios';
+
+const UserCard = () =>{
+
+  // use useState
+  const[users, setUsers] =useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const getUsers = ()=>{
+    setLoading(true);
+    // get api
+    axios.get('https://reqres.in/api/users?page=1')
+    // for scusesfull call
+    .then(res=>{
+      setLoading(false);
+      setUsers(res.data.data);
+    })
+    // for error
+    .catch(err =>{
+      console.log(err);
+      setLoading(false);
+    });
+
+  };
+
+
+  return(
+    <div className="UserCard">
+    {/* navigation bar */}
+    <nav className="navbar">
+      <h1 className="brand-name">Brand Name</h1>
+      <button className="btn-get-users" onClick={getUsers}>Get users</button>
+    </nav>
+    {/* wait for data */}
+    {loading ? (
+        <div className="loader">Loading...</div>
+      ) : (
+    <div className="grid">
+    {/* display user data from api */}
+    {users.map(user =>(
+      <div key={user.id} className="card">
+    <img src={user.avatar} alt={`${user.first_name} ${user.last_name}`}/>
+    <p className="name" >{`${user.first_name} ${user.last_name}`}</p>
+    <p className="email">{user.email}</p>
+    </div>
+    ))}
+    </div>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+
+
+
+export default UserCard;
+
